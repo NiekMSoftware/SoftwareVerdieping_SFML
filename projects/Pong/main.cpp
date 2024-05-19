dd374f3a640a105ab8aaeede2be623e5738fe98c
Niek Melet <113519331+NiekMSoftware@users.noreply.github.com>
2024-05-19 12:14:58 +0200

    Initial Project Setup
    Included everything the project would need.

---
+++ projects/Pong/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char* argv[])
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+    
+    return 0;
+}
