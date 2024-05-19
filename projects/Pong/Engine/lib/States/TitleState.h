ee47fe78f9559cb4b9cf9b34a5bfb8eea80fff47
Niek Melet <113519331+NiekMSoftware@users.noreply.github.com>
2024-05-19 22:14:08 +0200

    TitleState Update
    Implemented some visuals to the Title State and a timer to display text.

--- projects/Pong/Engine/lib/States/TitleState.h
+++ projects/Pong/Engine/lib/States/TitleState.h
@@ -11,4 +11,18 @@ public:
 	bool FixedUpdate(sf::Time fixedDt) override;
 	bool HandleEvent(const sf::Event& event) override;
 	void Draw() const override;
+
+private:
+	void LoadResources(const Context& context);
+
+private:
+	sf::Sprite mBackgroundSprite;
+	sf::Text mTitleText;
+	sf::Text mText;
+
+	sf::View mWorldView;
+	sf::FloatRect mWorldBounds;
+
+	bool mShowText;
+	sf::Time mTextTimer;
 };
--- projects/Pong/Engine/src/States/GameState.cpp
+++ projects/Pong/Engine/src/States/GameState.cpp
@@ -8,7 +8,9 @@ GameState::GameState(StateStack& stack, Context& context)
 
 void GameState::Draw() const
 {
+	GetContext().window->clear(sf::Color(34,44,55));
 
+	GetContext().window->display();
 }
 
 bool GameState::Update(sf::Time dt) 
@@ -23,5 +25,11 @@ bool GameState::FixedUpdate(sf::Time fixedDt)
 
 bool GameState::HandleEvent(const sf::Event& event) 
 {
+	if (event.type == sf::Event::Closed)
+	{
+		RequestStateClear();
+		return false;
+	}
+	
 	return true;
 }
