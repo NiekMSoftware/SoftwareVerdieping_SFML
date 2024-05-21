1b27699e1396d6540fa420a6f9b98e36a6c19791
Niek Melet <113519331+NiekMSoftware@users.noreply.github.com>
2024-05-21 11:05:00 +0200

    Created Project
    Also included all the necesary libraries

---
+++ SoftwareVerdieping/Pong Final Version/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <SFML/Graphics.hpp> 
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, World!");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+			}
+		}
+	}
+
+	return 0;
+}
